4a08fe91971195bee746483e2940ba11242ff2af
Toxe <toxe@toxe.net>
2020-03-06 18:55:36 +0100

    Add read_file.cpp

---
+++ read_file.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+int main()
+{
+    std::ifstream in("ships.json");
+    nlohmann::json data;
+
+    in >> data;
+
+    for (const auto& ship : data) {
+        std::cout << ship["category"].get<std::string>() << ", "
+                  << ship["ship_class"].get<std::string>() << ", "
+                  << ship["affiliation"].get<std::string>() << ", "
+                  << ship["manufacturer"].get<std::string>() << ", "
+                  << ship["model"].get<std::string>() << ", "
+                  << ship["length"].get<int>() << '\n';
+    }
+}
