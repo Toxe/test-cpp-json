f903518b4a19cb76cbd555f09cbee25d6ccc2007
Toxe <toxe@toxe.net>
2020-03-06 18:21:36 +0100

    Add prettify.cpp

---
+++ prettify.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+int main()
+{
+    std::ifstream in("ships.json");
+    nlohmann::json data;
+
+    in >> data;
+
+    std::cout << std::setw(2) << data << std::endl;
+}
